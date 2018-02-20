94e8ce27239104b4a9537e0129e14b37e008f35d
Lyky35 <36609323+Lyky35@users.noreply.github.com>
2018-02-20 11:57:47 -0600

    removed account permission requirement
    removed account permission requirement

--- data/talkactions/scripts/ghost.lua
+++ data/talkactions/scripts/ghost.lua
@@ -3,10 +3,6 @@ function onSay(player, words, param)
 		return true
 	end
 
-	if player:getAccountType() < ACCOUNT_TYPE_GOD then
-		return false
-	end
-
 	local position = player:getPosition()
 	local isGhost = not player:isInGhostMode()
 	player:setGhostMode(isGhost)
