156066a6db9f1850f977a3a1bb330a85dba084d3
Lyky35 <36609323+Lyky35@users.noreply.github.com>
2018-03-30 09:39:09 -0500

    Adding 3sec haste

--- data/spells/scripts/healing/heal friend.lua
+++ data/spells/scripts/healing/heal friend.lua
@@ -6,6 +6,11 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, 0)
 combat:setParameter(COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 
+local condition = Condition(CONDITION_HASTE)
+condition:setParameter(CONDITION_PARAM_TICKS, 3000)
+condition:setFormula(0.3, -35, 0.3, -35)
+combat:setCondition(condition)
+
 function onGetFormulaValues(cid, level, maglevel)
 	local min = math.max((base - variation), ((3 * maglevel + 2 * level) * (base - variation) / 100))
 	local max = math.max((base + variation), ((3 * maglevel + 2 * level) * (base + variation) / 100))
